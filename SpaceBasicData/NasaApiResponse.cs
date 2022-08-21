92273d62d8e49fe692ffc2b31c3b574864daaef2
sakuriver <sakuriver.redmine@gmail.com>
2022-08-21 09:10:45 +0900

    nasaのapiダウンロード(履歴編 part1

---
+++ SpaceBasicData/NasaApiResponse.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpaceBasicData
+{
+
+    public class NasaEpicHistoryRoot
+    {
+        [JsonPropertyName("histories")]
+        public List<NasaEpicDataResponse> dataHistories { get; set; }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NasaEpicDataResponse
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("stat_date")]
+        public string statDate { get; set; }
+
+        [JsonPropertyName("records")]
+        public List<NasaEpicDateRecord> epicDateRecords { get; set; }
+
+
+
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NasaEpicDateRecord
+    {
+        [JsonPropertyName("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonPropertyName("caption")]
+        public string Caption { get; set; }
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+
+
+        ///
+        /// 重心の場所
+        ///
+        [JsonPropertyName("centroid_coordinates")]
+        public NasaLocation CentroidCoordinates { get; set; }
+
+    }
+
+    public class NasaLocation
+    {
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+        
+        [JsonPropertyName("lon")]
+        public double Lon { get; set; }
+
+    }
+
+
+
+
+
+}
